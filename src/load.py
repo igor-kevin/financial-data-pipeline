c8cb45c81974157951639304949b9a6cc205c1a9
Igor Kevin <igor_kevin@hotmail.com>
2026-05-03 22:02:05 -0300

    feat: add database configuration and data loading functionality
    - Created .env.example for environment variable setup.
    - Updated .gitignore to exclude .env files.
    - Added dotenv and psycopg-binary to requirements.
    - Introduced docker-compose.yml for PostgreSQL service configuration.
    - Created SQL schema for financial_data table.
    - Implemented load.py to load data from parquet files into PostgreSQL.

---
+++ src/load.py
@@ -0,0 +1,67 @@
+import pandas as pd
+import psycopg
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+print(f"USER: {os.getenv('DB_USER')}")
+print(f"PASS: {os.getenv('DB_PASSWORD')}")
+print(f"HOST: {os.getenv('DB_HOST')}")
+print(f"PORT: {os.getenv('DB_PORT')}")
+print(f"NAME: {os.getenv('DB_NAME')}")
+
+def new_conn():
+    print('conectando')
+    return psycopg.connect(
+        host=os.getenv('DB_HOST'),
+        port=os.getenv('DB_PORT'),
+        dbname=os.getenv('DB_NAME'),
+        user=os.getenv('DB_USER'),
+        password=os.getenv('DB_PASSWORD')
+    )
+
+
+def load_gold_to_postgres():
+    df = pd.read_parquet("data/gold/metrics.parquet")
+
+    colunas = [ 
+        'date', 'ticker', 'taxa_pct_diaria', 'close_price', 'daily_return',
+        'retorno_30d', 'retorno_180d', 'retorno_365d',
+        'cdi_30d', 'pct_do_cdi_30d',
+        'cdi_180d', 'pct_do_cdi_180d',
+        'cdi_365d', 'pct_do_cdi_365d',
+        'drawdown'
+
+    ]
+    df = df.where(pd.notnull(df), None)
+    df = df[colunas]
+    print(df)
+    records = list(df.itertuples(index=False, name=None))
+    con = new_conn()
+    cursor = con.cursor()
+    query = '''
+        INSERT INTO financial_data  (
+            date, ticker, taxa_pct_diaria, close_price, daily_return,
+            retorno_30d, retorno_180d, retorno_365d,
+            cdi_30d, pct_do_cdi_30d,
+            cdi_180d, pct_do_cdi_180d,
+            cdi_365d, pct_do_cdi_365d,
+            drawdown
+        )
+        VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+        ON CONFLICT (date, ticker) DO NOTHING;
+    '''
+    cursor.executemany(query, records)
+    print('lel')
+    con.commit()
+    print(f' {cursor.rowcount} registros foram inseridos com sucesso')
+    cursor.close()
+    con.close()
+
+
+
+if __name__ == '__main__':
+    conexao = new_conn()
+    print('ok')
+    load_gold_to_postgres()
+    print('golded')
