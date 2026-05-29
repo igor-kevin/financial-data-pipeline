fc101154c7523e21f3dc34655359dd43195d03d8
Igor Kevin <igor_kevin@hotmail.com>
2026-05-29 11:19:48 -0300

    feat: add line chart for performance

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,16 +1,29 @@
 import { useResumo } from '../hooks/useResumo'
 import SummaryCard from '../components/cards/SummaryCard'
+import PerformanceChart from '../components/charts/PerformanceChart'
 
-const Dashboard = () => {
+
+const Dashboard = ({ticker}) => {
     const { resumo, loading, error } = useResumo()
-    if (loading) return <div>Carregando...</div>
-    if (error) return <div>Erro</div>
+
+    if (loading){
+        return <div>Carregando...</div>
+    }
+    if (error){
+        return <div>Erro</div>
+    }
 
     return (
-        <div className="grid grid-cols-3 gap-4 p-6">
-            {resumo.map(item => (
-                <SummaryCard key={item.ticker} data={item} />
-            ))}
+        <div className="p-6 flex flex-col gap-6">
+            <div className="grid grid-cols-3 gap-4">
+                {resumo.map(item => (
+                    <SummaryCard key={item.ticker} data={item} />
+                ))}
+            </div>
+
+            {ticker && (
+                <PerformanceChart ticker={ticker} />
+            )}
         </div>
     )
 }
